626f3f361569e8bdff880b7646a37658e4ccdfce
ravmangar <ravindra.mangar@my.uwi.edu>
2020-01-26 21:29:26 -0400

    Added python file to fetch 1000 tweets from TT and save to text file.

---
+++ get_tweets.py
@@ -0,0 +1,21 @@
+import tweepy
+
+ACCESS_TOKEN = "1115463269735653377-FNDF3x6irJ6Pzjc3zj3HzSXqbNKXvA"
+ACCESS_TOKEN_SECRET = "0ahplhRefK3Oa7VjuIKqrtEPFc1zC3DFVoExXDk8Stk2A"
+CONSUMER_KEY = "XMdcwozYFOlZh66BGVfd1Ukf3"
+CONSUMER_SECRET = "cTKsO4vGfyuLtCw3sdqHsbJSGqRpD62b5Rij8yE77F4GHLpqqG"
+
+auth = tweepy.OAuthHandler(CONSUMER_KEY, CONSUMER_SECRET)
+auth.set_access_token(ACCESS_TOKEN, ACCESS_TOKEN_SECRET)
+api = tweepy.API(auth, wait_on_rate_limit=True)
+
+print("Fetching tweets...")
+
+text_file = open("tweets.txt", "w")
+
+for tweet in tweepy.Cursor(api.search, lang="en", geocode="10.4576,-61.2414,90km").items(1000):
+    text_file.write(tweet.text + "\n")
+
+text_file.close()
+
+print("Done!\n", "Saved in tweets.txt")
