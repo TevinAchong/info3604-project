34b4d31ed3fd675a7022be296c4af3b58bd81708
tevin.achong <tevin.achong@my.uwi.edu>
2020-02-13 11:35:00 -0400

    linked firebase db

--- web_app/routes/index.js
+++ web_app/routes/index.js
@@ -1,17 +1,43 @@
-reviews = [
-    {
-    "id": 1,
-    "class": 1,
-    "text": "This is the absolute greatest movie ever, I love it!",
-    "trini": ""
-    },
-    {
-        "id": 2,
-        "class": 0,
-        "text": "This is the absolute worst movie ever. I hate it!",
-        "trini": ""
-    }
-];
+// reviews = [
+//     {
+//     "id": 1,
+//     "class": 1,
+//     "text": "This is the absolute greatest movie ever, I love it!",
+//     "trini": ""
+//     },
+//     {
+//         "id": 2,
+//         "class": 0,
+//         "text": "This is the absolute worst movie ever. I hate it!",
+//         "trini": ""
+//     }
+// ];
+
+var firebase = require('firebase');
+// Your web app's Firebase configuration
+var firebaseConfig = {
+    apiKey: "AIzaSyBiAwsFiDIHldyp2CFUzSF9qdmwiZC-r5M",
+    authDomain: "final-project-data-collection.firebaseapp.com",
+    databaseURL: "https://final-project-data-collection.firebaseio.com",
+    projectId: "final-project-data-collection",
+    storageBucket: "final-project-data-collection.appspot.com",
+    messagingSenderId: "320036326310",
+    appId: "1:320036326310:web:ae1cb4e32a6d53713c1d67"
+};
+  // Initialize Firebase
+firebase.initializeApp(firebaseConfig);
+
+const db = firebase.firestore();
+db.settings({timestampsInSnapshots:true});
+
+// Pull reviews from firebase and store them in reviews array
+var reviews = [];
+db.collection('reviews').onSnapshot(snapshot => {
+    let changes = snapshot.docChanges();
+    changes.forEach(change => {
+        reviews.push(change.doc.data());
+    });
+});
 
 exports.home = (req, res) => {
     res.render('home', {
